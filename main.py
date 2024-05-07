64ce41b49b6b5f97038b86f6a0865ba552c08312
Promise Temitope <oluwatobilobatemi05@gmail.com>
2024-05-07 12:43:12 +0000

    first from replit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+
+    
+
+def hello_world(x):
+    print(x)
+    return(x)
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'GET':
+        return render_template('index.html', hello_world=hello_world(print))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000)
